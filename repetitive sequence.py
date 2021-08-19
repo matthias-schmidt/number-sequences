3d12b276837670527d5f3a4ce797af2d4811303c
Matthias Schmidt <83017351+matthias-schmidt@users.noreply.github.com>
2021-08-20 01:26:17 +0200

    Add files via upload

--- repetitive sequence.py
+++ repetitive sequence.py
@@ -1,24 +1,3 @@
-from math import log10
-from datetime import datetime
-import tracemalloc
-
-
-# semifast:
-
-def repseq1(n):
-    F = [0, 1, 2, 2]
-    if n < 4: return F[n]
-    s, m, k = 3, 5, 3
-    while True:
-        F.extend([k for ctr in range(F[k])])
-        if m + k*F[k] > n: return s + (n-m+F[s+1]-1)//F[s+1]
-        s += F[k]
-        m += k*F[k]
-        k += 1
-
-
-# fast:
-        
 def repseq(n):
     if n < 12: return [0, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5][n]
     F = [0, 1, 2, 2]
@@ -37,30 +16,3 @@ def repseq(n):
         t += step*F[step]
         step += 1
         s, u = ss, uu
-
-from itertools import count
-
-
-
-def repseqF(n):
-    F = [0, 1, 2, 2]
-    if n < 4: return F[:n]
-    s = 3
-    m = 5
-    k = 3
-    while k <= n:
-        F.extend([k for ctr in range(F[k])])
-        k += 1
-    return F
-        
-
-n = 2**82
-
-
-starttime=datetime.now()
-#tracemalloc.start()
-print(repseq(n))
-#print(tracemalloc.get_traced_memory())
-#tracemalloc.stop()
-print(format(datetime.now()-starttime))
-
