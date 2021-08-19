ae24f9fdb436ca4869105662d384da748aff60db
Matthias Schmidt <83017351+matthias-schmidt@users.noreply.github.com>
2021-08-19 02:51:20 +0200

    Add files via upload

---
+++ fibonacci numbers.py
@@ -0,0 +1,47 @@
+from math import log2, log10
+from datetime import datetime
+
+def fibmult(a,b):
+    return [a[0]*b[0]+a[1]*b[1],a[0]*b[1]+a[1]*b[0]+a[1]*b[1]]
+
+def fibonacci(arg):
+    n = abs(arg)
+    if arg < 0: s = (-1)**(n%2+1)
+    else: s = 1
+    brg = bin(n+1)[3:].split('1')[::-1]                     # bin rep gaps
+    br = [len(brg[0])]+[len(gap)+1 for gap in brg[1:]]      # bin rep n+1 = 2**b[0] + 2**(b[0]+b[1]) + ...
+    a, f = [0,1], [1,0]                                     # fib seq seeds
+    for i in range(len(br)):
+        for k in range(br[i]): a = fibmult(a,a)
+        f = fibmult(a,f)
+    return s * f[0]
+
+
+n=1000000
+
+
+starttime=datetime.now()
+print(n,log10(fibonacci(n)))
+print(format(datetime.now()-starttime))
+
+
+
+# schlanker + etwas schneller:
+
+def fib(n):
+  if n < 0: return (-1)**(n % 2 + 1) * fib(-n)
+  a = b = x = 1
+  c = y = 0
+  while n:
+    if n % 2 == 0:
+      (a, b, c) = (a * a + b * b,
+                   a * b + b * c,
+                   b * b + c * c)
+      n /= 2
+    else:
+      (x, y) = (a * x + b * y,
+                b * x + c * y)
+      n -= 1
+  return y
+
+
---
+++ upside down numbers.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+import tracemalloc
+
+from itertools import product
+
+def complete(numbers,c):
+    n = len(numbers)
+    rot = {0:0, 1:1, 6:9, 8:8, 9:6}
+    if c == 0: return int(''.join([str(digit) for digit in numbers]+[str(rot[digit]) for digit in numbers[::-1]]))
+    else: return int(''.join([str(digit) for digit in numbers[:-1]]+[str(rot[numbers[-1]])]+[str(rot[digit]) for digit in numbers[:-1][::-1]]))
+    
+
+
+def udn(n):
+    nstr = str(n)
+
+    # count upside down numbers with less digits
+    ndig = [int(x) for x in str(n)]
+    nlen = len(ndig)
+    if nlen < 5: shift = [0, 3, 7, 19][nlen-1]
+    else: shift = 7+32*sum([5**i for i in range((nlen-1)//2-1)])+((nlen-1)%2)*12*5**((nlen-1)//2-1)
+
+    # count smaller upside down numbers with the same number of digits
+    less = [0,1,2,2,2,2,2,3,3,4]
+    lessc = [0,1,2,2,2,2,2,2,2,3]
+    if nlen == 1: return shift + lessc[ndig[0]]
+    count = (less[ndig[0]]-1) * 5**(nlen//2-1) * 3**(nlen%2)
+    i = 0
+    while i < nlen//2-1 and ndig[i] in [0, 1, 6, 8, 9]:
+        i += 1
+        count += less[ndig[i]] * 5**(nlen//2-1-i) * 3**(nlen%2)
+    if i == nlen//2-1:
+        if nlen%2 == 0:
+            if ndig[i] in [0, 1, 6, 8, 9] and complete(ndig[:nlen//2],0) < n: count += 1
+        else:
+            if ndig[i] in [0, 1, 6, 8, 9]:
+                count += lessc[ndig[nlen//2]]
+                if ndig[nlen//2] in [0, 1, 8] and complete(ndig[:nlen//2+1],1) < n: count += 1
+    return shift + count
+
+def udrange(n,m):
+    return udn(m) - udn(n)
+
+n = 7
+m = 10
+
+
+starttime=datetime.now()
+#tracemalloc.start()
+print(n,udn(n))
+print(m,udn(m))
+print(n,m,udrange(n,m))
+#print(tracemalloc.get_traced_memory())
+#tracemalloc.stop()
+print(format(datetime.now()-starttime))
+
